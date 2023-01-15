32392c65e5a03db3b577ad6ca2c6d1144fb23069
cjrincon27 <boloncahs@gmail.com>
2023-01-15 10:45:50 -0500

    6

--- src/app/pages/lotes/lotelist/lotelist.component.ts
+++ src/app/pages/lotes/lotelist/lotelist.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LotsService } from 'src/app/services/lots.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { debounceTime, Observable } from 'rxjs';
 import { Lot } from 'src/app/commons/interfaces/lot.interface';
 import { Router } from '@angular/router';
 
@@ -17,24 +17,26 @@ export class LotelistComponent implements OnInit {
   _lotService = inject(LotsService);
   lots$!: Observable<Lot[]>;
   searcher = new FormControl('');
-  _router=inject(Router);
+  _router = inject(Router);
 
   ngOnInit() {
     //this._lotService.getLot().subscribe((res) => console.log(res));
     this.lots$ = this._lotService.getLot();
-    this.searcher.valueChanges.subscribe(search => {
-      if (search) {
-        this.lots$ = this._lotService.getLot(search);
-      } else {
-        this.lots$ = this._lotService.getLot();
-      }
-    });
+    this.searcher.valueChanges.pipe(debounceTime(1000)).subscribe(search => {
+        console.log(search);
+
+        if (search) {
+          this.lots$ = this._lotService.getLot(search);
+        } else {
+          this.lots$ = this._lotService.getLot();
+        }
+      });
 
   }
-  editlote(lot :Lot){
+  editlote(lot: Lot) {
     this._router.navigateByUrl('lotes/edit');
   };
-  deletelote(lot :Lot){
+  deletelote(lot: Lot) {
     if (confirm(`Delete lot "${lot.name}" ?`)) {
       //this._lotService.deletelote(lot.id);
     }
--- src/app/services/lots.service.ts
+++ src/app/services/lots.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Firestore,addDoc,query,where,collectionData} from "@angular/fire/firestore";
-import {collection} from "@firebase/firestore";
+import { Firestore, addDoc, query, where, collectionData, getDocs,doc,updateDoc} from "@angular/fire/firestore";
+import { collection } from "@firebase/firestore";
 import { Observable } from 'rxjs';
 import { Lot } from '../commons/interfaces/lot.interface';
 
@@ -11,17 +11,29 @@ export class LotsService {
 
   constructor(private firestore: Firestore) { }
 
-  addlot(lot: Lot){
-    const lotRef = collection(this.firestore,'lots');
-    return addDoc(lotRef,lot);
+  addlot(lot: Lot) {
+    const lotRef = collection(this.firestore, 'lots');
+    return addDoc(lotRef, lot);
   }
 
-  getLot(filter=''){
-    const lotRef = collection(this.firestore,'lots');
-    let q= query(lotRef);
-    if(filter){
-      q= query(lotRef,where('name','==',filter));
+  getLot(filter = '') {
+    const lotRef = collection(this.firestore, 'lots');
+    let q = query(lotRef);
+    if (filter) {
+      q = query(lotRef, where('name', '==', filter));
     }
-    return collectionData(q) as unknown as Observable<Lot[]> ;
+    return collectionData(q) as unknown as Observable<Lot[]>;
   }
+
+  async updateLot(lot: Lot) {
+    const lotRef = collection(this.firestore, 'lost');
+    let q = query(lotRef, where('id', '==', lot.id));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach(async (document) => {
+      const docRef = doc(this.firestore, 'lots', document.id);
+      await updateDoc(docRef, { ...lot});
+    });
+  };
+  
 }
